3bed26686732e1d7598995bf8ff4ac6b939ffa3a
Meghana S P <meghana.poojary@aciworldwide.com>
2020-02-21 10:44:06 +0530

    commiting my aciwebapp

---
+++ src/test/java/com/aciwebapp/aciwebapp/AciwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aciwebapp.aciwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AciwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
